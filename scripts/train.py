2cc407c219f7b9ce1f720a462ef72d13a8ad2211
fan23j <fan23j@gmail.com>
2024-08-22 19:31:59 -0400

    save

--- scripts/train.py
+++ scripts/train.py
@@ -544,7 +544,6 @@ def main():
 
     # log prompts for pre-training ckpt
     first_global_step = start_epoch * num_steps_per_epoch + start_step
-
     write_sample(model, vae, scheduler_inference, cfg, start_epoch, exp_dir, first_global_step, dtype, device)
     log_sample(coordinator.is_master(), cfg, start_epoch, exp_dir, first_global_step)
     
